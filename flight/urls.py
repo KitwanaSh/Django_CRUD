64c7bd0f8c660c0f3c233583091463c950dab7f6
KitwanaSh <rodriguekitweze@gmail.com>
2023-11-05 15:13:06 +0000

    Fix: manage the form

--- flight/urls.py
+++ flight/urls.py
@@ -4,5 +4,6 @@ from . import views
 # This is the class for
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:flight_id>", views.flight, name="flight")
+    path("<int:flight_id>", views.flight, name="flight"),
+    path("<int:flight_id>/book", views.book, name="book")
 ]
